e0f19de54250552a49da48469bf060582ce70245
unknown <dev.joaovictor22@gmail.com>
2024-03-02 16:04:37 -0300

    feat: Added UI and sound effects; Code much more organized.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+/*
+*   Servidor http node.js feito do zero e revisado por mim mesmo (CodedBytes)
+*   Copyright original: CodedBytes
+*   Programado por: João Victor Segantini (CodedBytes) Sob a licensa MIT.
+*   OBS: É de suma importancia respeitar a licensa,e conforme a mesma, caso o projeto seja utilizado,
+*   é obrigatorio o devido copyright delegado pelo dono do projeto!
+*   Neste caso: manter esta mensagem e as localizações dos copyrights, originalmente inseridos no projeto, da forma que estão!
+*/
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
 
+// Configurações de comportamento e CORS
 app.use(cors());
 app.use(express.json());
 app.engine('html', require('ejs').renderFile);
@@ -10,49 +20,43 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
 
-
 /* 
-* Lista de itens com suas respectivas probabilidades de 0% a 100% de chances de sair
+* Lista de itens com suas respectivas probabilidades baseado na chance de sair
 * A quantidade maxima de itens pra esse evento, por enquanto, é de 8 itens,
-* Senbdo eles 4 itens consumiveis, 1 raro do update, 2 itens de cookie e 1 game ticket (pode ser outra coisa.)
+* Sendo eles 4 itens consumiveis, 1 raro do update, 2 itens de cookie e 1 game ticket(Obrigatorio)
 */
 const itens = [
-    { nome: 0, probabilidade: 98 }, // Item Consumivel 1
-    { nome: 1, probabilidade: 96 }, // Item Consumivel 2
-    { nome: 2, probabilidade: 94 }, // Item Consumivel 3
-    { nome: 3, probabilidade: 90 }, // Item Consumivel 4
-    { nome: 4, probabilidade: 12 }, // Arin Black Feather
-    { nome: 5, probabilidade: 47 }, // Water Aztec 
-    { nome: 6, probabilidade: 39 }, // Game Ticket
-    { nome: 7, probabilidade: 39 } // Removedor de cartas (Nunca falha).
+    { id: 0, chances: 98 }, // Item Consumivel 1
+    { id: 1, chances: 96 }, // Item Consumivel 2
+    { id: 2, chances: 94 }, // Item Consumivel 3
+    { id: 3, chances: 90 }, // Item Consumivel 4
+    { id: 4, chances: 12 }, // Arin Black Feather
+    { id: 5, chances: 47 }, // Water Aztec 
+    { id: 6, chances: 39 }, // Game Ticket
+    { id: 7, chances: 39 } // Removedor de cartas (Nunca falha).
 ];
 
 /* Função para selecionar um item com base na probabilidade */
-function sortearItem() {
-    const sorteio = Math.random() * 100; // Gera um número aleatório entre 0 e 100
-    let somaProbabilidades = 0;
-
+function sortearItem() 
+{
     // Calcula a soma total das probabilidades
-    for (const item of itens) {
-        somaProbabilidades += item.probabilidade;
-    }
+    let somaProbabilidades = 0;
+    for (const item of itens) { somaProbabilidades += item.chances; }
 
     // Sorteia um número entre 0 e a soma total das probabilidades
     let sorteioTotal = Math.random() * somaProbabilidades;
 
     // Encontra o item correspondente ao número sorteado
     for (const item of itens) {
-        if (sorteioTotal <= item.probabilidade) {
-            return item.nome; // Retorna o item sorteado
-        }
-        sorteioTotal -= item.probabilidade;
+        if (sorteioTotal <= item.chances) { return item.id; } // Retorna o item sorteado
+        sorteioTotal -= item.chances;
     }
 
     /*
     * Esta opção garante que, caso o sistema de probabilidade falhe,
     * o ticket usado retorne pro inventaro.   
     */
-    return itens[itens.length - 1].nome;
+    return itens[itens.length - 1].id;
 }
 
 // Pagina do jogo
@@ -62,8 +66,8 @@ app.get('/', (req, res) => { res.render(path.join(__dirname + '/public/game.ejs'
 app.get('/api/v1/sort_item', (req, res) => {
     // Mandando o json com o item pro cliente
     const item = sortearItem();
-    console.log(item);
     res.status(200).json({ item_sorted: item });
 });
 
+// Servindo o webapp
 app.listen(3000, () => console.log('Aplicação rodando na porta 3000!'));
